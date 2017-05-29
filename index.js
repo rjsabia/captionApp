a0a9a48364263bd27bfc887a557f5ef991a27153
Russell Sabia <rjsabia@gmail.com>
2017-05-29 02:49:44 -0700

    S3 working again, update aws creds, no rekog yet

--- index.js
+++ index.js
@@ -1,59 +1,60 @@
- const ImageAnalyser = require('./imageAnalyser');
- var express = require('express')
+const ImageAnalyser = require('./imageAnalyser');
+var express = require('express')
     , router = express()
     , aws = require('aws-sdk');
 var path = require('path');
-  router.get('/', function(req, res) {
+
+router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname,'./index.html'))
   })
 
-  var AWS_ACCESS_KEY = 'AKIAIA5P4GULWT6ZZB6Q'
-  var AWS_SECRET_KEY = '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'
-  var S3_BUCKET = 'rjs-rekog-test-bucket-2'
+var AWS_ACCESS_KEY = 'AKIAJZRUPRYFXPUG7U4Q';
+var AWS_SECRET_KEY = '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';
+var S3_BUCKET = 'rjs-rekog-test-bucket-2';
 
-  router.get('/sign', function(req, res) {
-    aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
+router.get('/sign', function(req, res) {
+  aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
 
-    var s3 = new aws.S3()
-    var options = {
-      Bucket: S3_BUCKET,
-      Key: req.query.file_name,
-      Expires: 60,
-      ContentType: req.query.file_type,
-      ACL: 'public-read'
-    }
+  var s3 = new aws.S3()
+  var options = {
+    Bucket: S3_BUCKET,
+    Key: req.query.file_name,
+    Expires: 60,
+    ContentType: req.query.file_type,
+    ACL: 'public-read'
+  }
 
-    s3.getSignedUrl('putObject', options, function(err, data){
-      if(err) return res.send('Error with S3')
-      return res.json({
-              signed_request: data,
-              url: 'https://s3.amazonaws.com/' + S3_BUCKET + '/' + req.query.file_name,
+  s3.getSignedUrl('putObject', options, function(err, data){
+    if(err) return res.send('Error with S3')
+      
+      res.json({
+        signed_request: data,
+        url: 'https://s3.amazonaws.com/' + S3_BUCKET + '/' + req.query.file_name,
         
-            })
+      })
       //add Rekog here
-
-         
     })
   })
-router.get('/rekog', function(req, res){
-  aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
-  const s3Config = {
-            bucket: S3_BUCKET,
-            imageName: req.query.file_name,
-          };
+// router.get('/rekog', function(req, res){
+//   aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
+  
+//   const s3Config = {
+//             bucket: S3_BUCKET,
+//             imageName: req.query.file_name,
+//           };
 
-          return ImageAnalyser
-            .getImageLabels(s3Config)
-            .then((labels) => {
-              res.json({
-                url: 'https://s3.amazonaws.com/' + S3_BUCKET + '/' + req.query.file_name,
-                labels: labels
-              })
-            })
-            .catch((error) => {
-              res.status(500).send(error);
-            });
-          })
+//           return ImageAnalyser
+//             .getImageLabels(s3Config)
+//             .then((labels) => {
+//               res.json({
+//                 url: 'https://s3.amazonaws.com/' + S3_BUCKET + '/' + req.query.file_name,
+//                 Labels: labels
+//               })
+//             })
+//             .catch((error) => {
+//               res.status(500).send(error);
+//             });
+//           })
 
 
 router.listen(3000);
