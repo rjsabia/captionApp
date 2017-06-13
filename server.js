24cb454aa825343843a77b0bd509832eb8da814c
Russell Sabia <rjsabia@gmail.com>
2017-06-12 17:54:13 -0700

    added all files for scotchIO authentification, no function yet, need to link database still

--- server.js
+++ server.js
@@ -1,63 +1,47 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
+// server.js
 
-const {router: usersRouter} = require('./users');
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
 
-mongoose.Promise = global.Promise;
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
 
-const {PORT, DATABASE_URL} = require('./config');
+var configDB = require('./config/database.js');
 
-const app = express();
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
 
-// logging
-app.use(morgan('common'));
+require('./config/passport')(passport); // pass passport for configuration
 
-app.use('/users/', usersRouter);
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('*', function(req, res) {
-  return res.status(404).json({message: 'Not Found'});
-});
+app.set('view engine', 'ejs'); // set up ejs for templating
 
-// referenced by both runServer and closeServer. closeServer
-// assumes runServer has run and set `server` to a server object
-let server;
+// required for passport
+app.use(session({
+    secret: 'ilovescotchscotchyscotchscotch', // session secret
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
-function runServer() {
-  return new Promise((resolve, reject) => {
-    mongoose.connect(DATABASE_URL, err => {
-      if (err) {
-        return reject(err);
-      }
-      server = app.listen(PORT, () => {
-        console.log(`Your app is listening on port ${PORT}`);
-        resolve();
-      })
-      .on('error', err => {
-        mongoose.disconnect();
-        reject(err);
-      });
-    });
-  });
-}
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
-function closeServer() {
-  return mongoose.disconnect().then(() => {
-     return new Promise((resolve, reject) => {
-       console.log('Closing server');
-       server.close(err => {
-           if (err) {
-               return reject(err);
-           }
-           resolve();
-       });
-     });
-  });
-}
-
-if (require.main === module) {
-  runServer().catch(err => console.error(err));
-};
-
-module.exports = {app, runServer, closeServer};
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
